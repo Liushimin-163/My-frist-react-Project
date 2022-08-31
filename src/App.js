ef00e471a1227b8f3e20c06b24a539b4e66189a9
Liu shimin <liushimin_163@163.com>
2022-08-31 19:24:26 +0900

    Add files via upload
    create base on react project.
    Movie land. it is for searching movies

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import MovieCard from './MovieCard';
+import SearchIcon from './search.svg';
+
+
+//c2ef98fd
+const API_URI='http://www.omdbapi.com?apikey=c2ef98fd';
+
+
+const App =()=> {
+  const[movies, setMovies]=useState([]);
+  const[searchTerm,setSearchTerm]=useState('');
+  const searchMovies =async(title)=>{
+    const response= await fetch(`${API_URI}&s=${title}`);
+    const data=await response.json();
+    setMovies(data.Search);
+  }
+  useEffect(()=>{
+    searchMovies("Spiderman");
+  },[]);
+    return (
+    
+    <div className="app">
+      <h1>MovieLand</h1>
+      <div className="search">
+        <input placeholder="Search for movies"
+               value={searchTerm}
+               onChange={(e)=>setSearchTerm(e.target.value)}
+        />
+        <img
+         src={SearchIcon}
+         alt="search"
+         onClick={()=>searchMovies(searchTerm)}
+        />
+      </div>
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+       
+        <div className='empty'>
+          <h2>no moive has been found</h2>
+        </div>
+      )}
+     
+    </div>
+   
+  );
+}
+
+export default App;
---
+++ src/MainContent.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+const MainContent=()=>{
+    return(
+        <div className="content">
+           <h2>I am learning React.</h2>
+         </div> 
+    );
+}
+export default MovieCard;
